e1ebbbe463edc59bf5e2898e354b663d8de37084
lisab1993 <brown.lisa999@gmail.com>
2023-06-11 23:07:00 -0700

    converts easy decimals

--- build_calc/src/components/DecimalHelp.jsx
+++ build_calc/src/components/DecimalHelp.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 const DecimalHelp = () => {
-  const [decimalEntry, setDecimalEntry] = useState(0.375);
+  const [decimalEntry, setDecimalEntry] = useState(5.375);
 
   const handleDecimalChange = (e) => {
     setDecimalEntry(e.target.value);
@@ -14,6 +14,7 @@ const DecimalHelp = () => {
   };
 
   const toFraction = (num) => {
+    // this function will not serve its purpose here when applied to decimal numbers larger than 1 (0.5 is okay; 5.5 is not okay)
     // this function came from redteam-snippets on github
     // original code: https://gist.github.com/redteam-snippets/3934258
     let top = num.toString().replace(/\d+[.]/, "");
@@ -23,12 +24,43 @@ const DecimalHelp = () => {
     }
     let x = gcd(top, bottom);
     return top / x + "/" + bottom / x;
-
   };
 
+ 
+  const mixedFraction = (num) => {
+    //1. pull out the whole number, if applicable
+    //2. don't change it. 
+    //3. everything after the decimal will put use the toFraction function
+    let wholeNum = ""
+    let decimalPart = ""
+    if (num >= 1){
+        //I need every number to the left of the decimal, in order
+        //convert the number to a string
+        let textNum = num.toString()
+        //split the string into an array
+        let textNumArr = textNum.split('')
+        //find the index of the decimal
+        let decimalIndex = textNumArr.indexOf('.')
+        //create an array from the numbers to the left of the decimal
+        let wholeNumArr = textNumArr.slice(0, decimalIndex)
+        //convert whe whole number array into a string
+        wholeNum = wholeNumArr.join('')
+        //convert the whole number string into a number
+        wholeNum = parseInt(textNum)
+        decimalPart = num - wholeNum
+        let fractionPart = toFraction(decimalPart)
+        let output = wholeNum + " ~ " + fractionPart
+        // let decimalFraction = toFraction()
+        return output
+    }else {
+        return toFraction(decimalEntry)
+    }
+
+  }
+
   const handleSubmit = async (e) => {
-    console.log(toFraction(decimalEntry));
-    console.log(decimalEntry, " - decimalEntry state right now");
+    console.log(mixedFraction(decimalEntry), 'mixed fraction output')
+    // console.log(decimalEntry, " - decimalEntry state right now");
   };
 
   return (
